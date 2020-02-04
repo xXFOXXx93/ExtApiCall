423da8188c4eaeb26772388c97b5f00e388a0c57
Andres <andres.ricci93@outlook.com>
2020-02-04 12:30:09 +0100

    Initial commit

---
+++ src/main/java/com/springframework/ExtApiCall/ExtApiCallApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.ExtApiCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtApiCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtApiCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/ExtApiCall/ExtApiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.ExtApiCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtApiCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
